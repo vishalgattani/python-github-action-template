d6d33be27624f5099066687c490392281d227e3b
Vishal Gattani <vishalgattani09@gmail.com>
2024-11-12 22:40:05 -0500

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,33 @@
+import logging
+import logging.handlers
+
+
+def setup_logger(name, level=logging.DEBUG):
+    """Set up a logger that writes to both a log file and the console."""
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    if not logger.hasHandlers():
+        file_handler = logging.handlers.RotatingFileHandler(
+            name,
+            maxBytes=1024 * 1024,
+            backupCount=1,
+            encoding="utf8",
+        )
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+        )
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+    return logger
+
+
+# logger = setup_logger()
+
+# Example usage
+# logger.info("This is an info message from the weather API")
+# logger.error("This is an error message from the weather API")
